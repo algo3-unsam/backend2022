a9017896ed946d68ca757b733baf65774466a9fc
sebas <sebidark97@gmail.com>
2022-03-20 19:29:04 -0300

    Actualizando main y poniendo tests

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,11 @@
-import java.util.*
-class Usuario(var Nombre: String, var Apellido: String, var Username: String, var fechaDeAlta: Int, var paisDeResidencia: String){
-    fun antiguedad() = Calendar.YEAR - fechaDeAlta
 
-    fun descuentoPorAntiguedad() = if(antiguedad() > 15) 15 else antiguedad()
+import java.time.LocalDate
+import java.time.Period
+
+class Usuario(var Nombre: String, var Apellido: String, var Username: String, var fechaDeAlta: String, var paisDeResidencia: String){
+    fun antiguedad() = Period.between(LocalDate.parse(fechaDeAlta), LocalDate.now())
+
+    fun descuentoPorAntiguedad() = if(antiguedad().years > 15) 15 else antiguedad().years
 }
 
 class Destino(var Pais: String, var Ciudad: String, var costoBase: Float){
--- src/test/kotlin/TestSebas.kt
+++ src/test/kotlin/TestSebas.kt
@@ -1,2 +1,24 @@
-class TestSebas {
-}
+import io.kotest.core.spec.IsolationMode
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+
+class TestMios: DescribeSpec ({
+    isolationMode = IsolationMode.InstancePerTest
+    val usuario1 = Usuario(Apellido = "Rodrigues", Username = "El_Perri", Nombre = "Sebas", fechaDeAlta = "2005-07-20", paisDeResidencia = "Argentina")
+    val usuario2 = Usuario(Apellido = "Martinez", Username = "Holu204", Nombre = "Camilo", fechaDeAlta = "2018-11-05", paisDeResidencia = "Brasil")
+    describe("Usuario con Antiguedad y Local"){
+        val destino1 = Destino(Pais = "Argentina", Ciudad = "BuenosAires", costoBase = 3000F)
+        describe("destino local"){
+            destino1.esLocal() shouldBe true
+            destino1.precio(usuario1) shouldBe 2550.0
+        }
+    }
+    describe("Usuario con Poca Antiguedad y no Local"){
+        val destino2 = Destino(Pais = "Colombia", Ciudad = "Bogota", costoBase = 25000F)
+        describe("Destino No Local"){
+            destino2.esLocal() shouldBe false
+            destino2.precio(usuario2) shouldBe 29250.0
+        }
+    }
+
+})
