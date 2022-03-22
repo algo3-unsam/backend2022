9f08b0a9fe6b1cff5f5b5c51fb266fffd8b9293f
Rodrigo <rodrigoalonso2000@gmail.com>
2022-03-22 15:51:18 -0300

    arreglado descuento por local y correccion de test

--- src/test/kotlin/Testdestinos.kt
+++ src/test/kotlin/Testdestinos.kt
@@ -7,13 +7,19 @@ import java.time.Period
 class Testdestinos: DescribeSpec ({
     isolationMode = IsolationMode.InstancePerTest
     val usuario1 = Usuario(Apellido = "Torres", Username = "T20", Nombre = "Nicolas", fechaDeAlta = "2010-01-15", paisDeResidencia = "Alemania")
-    describe("Usuario con mucha antiguedad no local"){
+    describe("Usuario con poca antiguedad no local"){
         val destino1 = Destino(Pais = "Francia", Ciudad = "Paris", costoBase = 20000F)
         describe("destino no local, se paga un extra por el pasaje"){
             destino1.esLocal() shouldBe false
-            destino1.precio(usuario1) shouldBe 21600
+            destino1.precio(usuario1) shouldBe 24000
 
         }
+    }
+    val usuario2 = Usuario(Apellido = "Martinez", Username = "Martu50", Nombre = "Martin", fechaDeAlta = "2005-05-07", paisDeResidencia = "Argentina")
+    describe("Usuario local con mucha antiguedad"){
+        val destino2 = Destino(Pais = "Argentina", Ciudad = "Buenos aires", costoBase = 10000F)
+        destino2.esLocal() shouldBe true
+        destino2.precio(usuario2) shouldBe 8500
     }
 
 })
