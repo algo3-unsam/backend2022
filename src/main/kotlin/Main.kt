c7e1662e967ce07729bca8d8ea105e779cb9ff75
LeandroAmarilla12 <leandroamarilla50@gmail.com>
2022-03-22 14:25:56 -0300

    Test terminados

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,12 @@
-
+import java.math.BigDecimal
 import java.time.LocalDate
-import java.time.LocalDateTime
+import java.time.Period
+import java.time.Year
 import java.util.*
+class Usuario(var Nombre: String, var Apellido: String, var Username: String, var fechaDeAlta: String, var paisDeResidencia: String){
+    fun antiguedad() = Period.between(LocalDate.parse(fechaDeAlta), LocalDate.now())
 
-
-class Usuario(var Nombre: String, var Apellido: String, var Username: String, var fechaDeAlta: CharSequence, var paisDeResidencia: String){
-    fun antiguedad() = (LocalDate.now() - LocalDate.parse(fechaDeAlta)).year
-
-    fun descuentoPorAntiguedad() = if(antiguedad() > 15) 15 else antiguedad()
+    fun descuentoPorAntiguedad() = if(antiguedad().years > 15) 15 else antiguedad().years
 }
 
 class Destino(var Pais: String, var Ciudad: String, var costoBase: Float){
@@ -18,5 +17,5 @@ class Destino(var Pais: String, var Ciudad: String, var costoBase: Float){
 
     fun porcentajeDestino() = if (!esLocal()) costoBase * 0.2 else 0.0
 
-    fun descuento(unUsuario: Usuario) = costoBase * (unUsuario.descuentoPorAntiguedad()*0.01)
+    fun descuento(unUsuario: Usuario) = if (unUsuario.paisDeResidencia == Pais) (costoBase * (unUsuario.descuentoPorAntiguedad()*0.01)) else 0.0
 }
