c1a804b3abd056efca7ca06440431311f6a17888
LeandroAmarilla12 <leandroamarilla50@gmail.com>
2022-03-22 15:16:45 -0300

    Revert "Corrijo identacion"
    This reverts commit 33e68c866053cb3ec619567fffcdc643659ae5c6.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,20 +3,13 @@ import java.time.LocalDate
 import java.time.Period
 import java.time.Year
 import java.util.*
-
-class Usuario(
-    var Nombre: String,
-    var Apellido: String,
-    var Username: String,
-    var fechaDeAlta: String,
-    var paisDeResidencia: String
-) {
+class Usuario(var Nombre: String, var Apellido: String, var Username: String, var fechaDeAlta: String, var paisDeResidencia: String){
     fun antiguedad() = Period.between(LocalDate.parse(fechaDeAlta), LocalDate.now())
 
-    fun descuentoPorAntiguedad() = if (antiguedad().years > 15) 15 else antiguedad().years
+    fun descuentoPorAntiguedad() = if(antiguedad().years > 15) 15 else antiguedad().years
 }
 
-class Destino(var Pais: String, var Ciudad: String, var costoBase: Float) {
+class Destino(var Pais: String, var Ciudad: String, var costoBase: Float){
 
     fun esLocal() = Pais == "Argentina"
 
@@ -24,6 +17,5 @@ class Destino(var Pais: String, var Ciudad: String, var costoBase: Float) {
 
     fun porcentajeDestino() = if (!esLocal()) costoBase * 0.2 else 0.0
 
-    fun descuento(unUsuario: Usuario) =
-        if (unUsuario.paisDeResidencia == Pais) (costoBase * (unUsuario.descuentoPorAntiguedad() * 0.01)) else 0.0
+    fun descuento(unUsuario: Usuario) = if (unUsuario.paisDeResidencia == Pais) (costoBase * (unUsuario.descuentoPorAntiguedad()*0.01)) else 0.0
 }
