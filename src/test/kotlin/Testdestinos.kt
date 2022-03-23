18d9ed842fc9a54d7352b86b37cd6223a181f6a6
LeandroAmarilla12 <leandroamarilla50@gmail.com>
2022-03-23 15:08:03 -0300

    Arreglado Issues y mejoras al codigo

--- src/test/kotlin/Testdestinos.kt
+++ src/test/kotlin/Testdestinos.kt
@@ -2,22 +2,21 @@ import io.kotest.core.spec.IsolationMode
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 import java.time.LocalDate
-import java.time.Period
 
 class Testdestinos: DescribeSpec ({
     isolationMode = IsolationMode.InstancePerTest
-    val usuario1 = Usuario(Apellido = "Torres", Username = "T20", Nombre = "Nicolas", fechaDeAlta = "2010-01-15", paisDeResidencia = "Alemania")
+    val usuario1 = Usuario(apellido = "Torres", username = "T20", nombre = "Nicolas", fechaDeAlta = LocalDate.of(2010,1,15), paisDeResidencia = "Alemania")
     describe("Usuario con poca antiguedad no local"){
-        val destino1 = Destino(Pais = "Francia", Ciudad = "Paris", costoBase = 20000F)
+        val destino1 = Destino(pais = "Francia", ciudad = "Paris", costoBase = 20000F)
         describe("destino no local, se paga un extra por el pasaje"){
             destino1.esLocal() shouldBe false
             destino1.precio(usuario1) shouldBe 24000
 
         }
     }
-    val usuario2 = Usuario(Apellido = "Martinez", Username = "Martu50", Nombre = "Martin", fechaDeAlta = "2005-05-07", paisDeResidencia = "Argentina")
+    val usuario2 = Usuario(apellido = "Martinez", username = "Martu50", nombre = "Martin", fechaDeAlta = LocalDate.of(2005,5,7), paisDeResidencia = "Argentina")
     describe("Usuario local con mucha antiguedad"){
-        val destino2 = Destino(Pais = "Argentina", Ciudad = "Buenos aires", costoBase = 10000F)
+        val destino2 = Destino(pais = "Argentina", ciudad = "Buenos aires", costoBase = 10000F)
         destino2.esLocal() shouldBe true
         destino2.precio(usuario2) shouldBe 8500
     }
